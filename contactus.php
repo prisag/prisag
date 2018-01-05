49e4a9ff16e0ed92eaa916daf1660c718535f61c
Programming Wizard <wizard.of.programming@gmail.com>
2018-01-05 15:51:19 +0000

    new update

--- contactus.php
+++ contactus.php
@@ -2,52 +2,37 @@
 	
 		$name = $_POST['name'];
 		$email = $_POST['email'];
-                $tel = $_POST['tel'];
 		$message = $_POST['message'];
-                $check = $_POST['check'];
-		$from = 'teslavnit.tk'; 
-		$to = 'teslaclubofinnovations@gmail.com'; 
-		$subject = 'Message from teslavnit.tk ';
+		$from = 'prisag.tk'; 
+		$to = 'prisag.vnit@gmail.com'; 
+		$subject = 'Message from PriSag.tk ';
 		
 		
-
-                if(isset($_POST['check'])) 
-                {
-                $body = " From: $name\n E-Mail: $email\n Phone: $tel\n\n Message:\n$message \n I would like to receive notifications";
+                
+                $body = " From: $name\n E-Mail: $email\n\n Message:\n$message \n";
                 mail ($to, $subject, $body, $from);
-                }
-                else 
-                {
-                $body = " From: $name\n E-Mail: $email\n Phone: $tel\n\n Message:\n$message \n I would not like to receive notifications";
-                mail ($to, $subject, $body, $from);
-                }
-	$reply_body = "\nHello $name, \n\n\tThank you for contacting us. \n\tStay tuned for more updates. \n\nLove, TeslaBot";
-        $reply_subject = "Reply: Tesla Club of Innovations";
+                
+	$reply_body = "\nHello $name, \n\n\tThank you for contacting us. \n\tStay tuned for more updates. \n\nLove, PriSag-Bot";
+        $reply_subject = "Reply: PriSag";
         mail ($email, $reply_subject, $reply_body, $from);
-
-
 $servername = "localhost";
-$username = "id2184138_teslavnitdb";
-$password = "TeslaVNITdb";
-$dbname = "id2184138_teslavnitdb";
+$username = "id2286154_prisag";
+$password = "prisag";
+$dbname = "id2286154_prisag";
 $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
-
-$sql = "INSERT INTO users (name, email, phone)
-VALUES ('$name', '$email', '$tel')";
-
+$sql = "INSERT INTO users (name, email, message)
+VALUES ('$name', '$email', '$message')";
 if ($conn->query($sql) === TRUE) {
     echo "New record created successfully";
 } else {
     echo "Error: " . $sql . "<br>" . $conn->error;
 }
-
 $conn->close();
-
 ?>
 <br>
-<a href = "contactus.html" >Go Back</a>
-<meta http-equiv="refresh" content="0;URL=contactus.html" />
+<a href = "index.html" >Go Back</a>
+<meta http-equiv="refresh" content="0;URL=index.html" />
