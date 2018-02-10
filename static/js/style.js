713b81245cafa2a7664631d6e97c95bc031c225b
Sagar Bansal <sagarbansal099@gmail.com>
2018-02-10 17:45:39 +0000

    animation add prisag/ contact bug fix

--- static/js/style.js
+++ static/js/style.js
@@ -1,8 +1,231 @@
 $(function new1 (){
-    
+
 	        $("#button").click(function(){
 			    $("#hire_mail").toggle();
 	        });
-            
-            
+
+
 });
+
+var curry = function curry(f) {
+  return function () {
+    for (var _len = arguments.length, args = Array(_len), _key = 0; _key < _len; _key++) {if (window.CP.shouldStopExecution(1)){break;}if (window.CP.shouldStopExecution(1)){break;}if (window.CP.shouldStopExecution(1)){break;}if (window.CP.shouldStopExecution(1)){break;}if (window.CP.shouldStopExecution(1)){break;}if (window.CP.shouldStopExecution(1)){break;}if (window.CP.shouldStopExecution(1)){break;}if (window.CP.shouldStopExecution(1)){break;}if (window.CP.shouldStopExecution(1)){break;}if (window.CP.shouldStopExecution(1)){break;}if (window.CP.shouldStopExecution(1)){break;}if (window.CP.shouldStopExecution(1)){break;}if (window.CP.shouldStopExecution(1)){break;}if (window.CP.shouldStopExecution(1)){break;}if (window.CP.shouldStopExecution(1)){break;}if (window.CP.shouldStopExecution(1)){break;}if (window.CP.shouldStopExecution(1)){break;}if (window.CP.shouldStopExecution(1)){break;}if (window.CP.shouldStopExecution(1)){break;}if (window.CP.shouldStopExecution(1)){break;}if (window.CP.shouldStopExecution(1)){break;}if (window.CP.shouldStopExecution(1)){break;}if (window.CP.shouldStopExecution(1)){break;}if (window.CP.shouldStopExecution(1)){break;}if (window.CP.shouldStopExecution(1)){break;}if (window.CP.shouldStopExecution(1)){break;}if (window.CP.shouldStopExecution(1)){break;}if (window.CP.shouldStopExecution(1)){break;}if (window.CP.shouldStopExecution(1)){break;}if (window.CP.shouldStopExecution(1)){break;}if (window.CP.shouldStopExecution(1)){break;}if (window.CP.shouldStopExecution(1)){break;}
+      args[_key] = arguments[_key];
+    }
+window.CP.exitedLoop(1);
+
+window.CP.exitedLoop(1);
+
+window.CP.exitedLoop(1);
+
+window.CP.exitedLoop(1);
+
+window.CP.exitedLoop(1);
+
+window.CP.exitedLoop(1);
+
+window.CP.exitedLoop(1);
+
+window.CP.exitedLoop(1);
+
+window.CP.exitedLoop(1);
+
+window.CP.exitedLoop(1);
+
+window.CP.exitedLoop(1);
+
+window.CP.exitedLoop(1);
+
+window.CP.exitedLoop(1);
+
+window.CP.exitedLoop(1);
+
+window.CP.exitedLoop(1);
+
+window.CP.exitedLoop(1);
+
+window.CP.exitedLoop(1);
+
+window.CP.exitedLoop(1);
+
+window.CP.exitedLoop(1);
+
+window.CP.exitedLoop(1);
+
+window.CP.exitedLoop(1);
+
+window.CP.exitedLoop(1);
+
+window.CP.exitedLoop(1);
+
+window.CP.exitedLoop(1);
+
+window.CP.exitedLoop(1);
+
+window.CP.exitedLoop(1);
+
+window.CP.exitedLoop(1);
+
+window.CP.exitedLoop(1);
+
+window.CP.exitedLoop(1);
+
+window.CP.exitedLoop(1);
+
+window.CP.exitedLoop(1);
+
+window.CP.exitedLoop(1);
+
+
+    return args.length >= f.length ? f.apply(undefined, args) : curry(f.bind.apply(f, [f].concat(args)));
+  };
+};
+
+var compose = function compose(f, g) {
+  return function (x) {
+    return f(g(x));
+  };
+};
+var composeN = function composeN() {
+  for (var _len2 = arguments.length, fns = Array(_len2), _key2 = 0; _key2 < _len2; _key2++) {if (window.CP.shouldStopExecution(2)){break;}if (window.CP.shouldStopExecution(2)){break;}if (window.CP.shouldStopExecution(2)){break;}if (window.CP.shouldStopExecution(2)){break;}if (window.CP.shouldStopExecution(2)){break;}if (window.CP.shouldStopExecution(2)){break;}if (window.CP.shouldStopExecution(2)){break;}if (window.CP.shouldStopExecution(2)){break;}if (window.CP.shouldStopExecution(2)){break;}if (window.CP.shouldStopExecution(2)){break;}if (window.CP.shouldStopExecution(2)){break;}if (window.CP.shouldStopExecution(2)){break;}if (window.CP.shouldStopExecution(2)){break;}if (window.CP.shouldStopExecution(2)){break;}if (window.CP.shouldStopExecution(2)){break;}if (window.CP.shouldStopExecution(2)){break;}if (window.CP.shouldStopExecution(2)){break;}if (window.CP.shouldStopExecution(2)){break;}if (window.CP.shouldStopExecution(2)){break;}if (window.CP.shouldStopExecution(2)){break;}if (window.CP.shouldStopExecution(2)){break;}if (window.CP.shouldStopExecution(2)){break;}if (window.CP.shouldStopExecution(2)){break;}if (window.CP.shouldStopExecution(2)){break;}if (window.CP.shouldStopExecution(2)){break;}if (window.CP.shouldStopExecution(2)){break;}if (window.CP.shouldStopExecution(2)){break;}if (window.CP.shouldStopExecution(2)){break;}if (window.CP.shouldStopExecution(2)){break;}if (window.CP.shouldStopExecution(2)){break;}if (window.CP.shouldStopExecution(2)){break;}if (window.CP.shouldStopExecution(2)){break;}
+    fns[_key2] = arguments[_key2];
+  }
+window.CP.exitedLoop(2);
+
+window.CP.exitedLoop(2);
+
+window.CP.exitedLoop(2);
+
+window.CP.exitedLoop(2);
+
+window.CP.exitedLoop(2);
+
+window.CP.exitedLoop(2);
+
+window.CP.exitedLoop(2);
+
+window.CP.exitedLoop(2);
+
+window.CP.exitedLoop(2);
+
+window.CP.exitedLoop(2);
+
+window.CP.exitedLoop(2);
+
+window.CP.exitedLoop(2);
+
+window.CP.exitedLoop(2);
+
+window.CP.exitedLoop(2);
+
+window.CP.exitedLoop(2);
+
+window.CP.exitedLoop(2);
+
+window.CP.exitedLoop(2);
+
+window.CP.exitedLoop(2);
+
+window.CP.exitedLoop(2);
+
+window.CP.exitedLoop(2);
+
+window.CP.exitedLoop(2);
+
+window.CP.exitedLoop(2);
+
+window.CP.exitedLoop(2);
+
+window.CP.exitedLoop(2);
+
+window.CP.exitedLoop(2);
+
+window.CP.exitedLoop(2);
+
+window.CP.exitedLoop(2);
+
+window.CP.exitedLoop(2);
+
+window.CP.exitedLoop(2);
+
+window.CP.exitedLoop(2);
+
+window.CP.exitedLoop(2);
+
+window.CP.exitedLoop(2);
+
+
+  return function () {
+    for (var _len3 = arguments.length, args = Array(_len3), _key3 = 0; _key3 < _len3; _key3++) {if (window.CP.shouldStopExecution(3)){break;}if (window.CP.shouldStopExecution(3)){break;}if (window.CP.shouldStopExecution(3)){break;}if (window.CP.shouldStopExecution(3)){break;}if (window.CP.shouldStopExecution(3)){break;}if (window.CP.shouldStopExecution(3)){break;}if (window.CP.shouldStopExecution(3)){break;}if (window.CP.shouldStopExecution(3)){break;}if (window.CP.shouldStopExecution(3)){break;}if (window.CP.shouldStopExecution(3)){break;}if (window.CP.shouldStopExecution(3)){break;}if (window.CP.shouldStopExecution(3)){break;}if (window.CP.shouldStopExecution(3)){break;}if (window.CP.shouldStopExecution(3)){break;}if (window.CP.shouldStopExecution(3)){break;}if (window.CP.shouldStopExecution(3)){break;}if (window.CP.shouldStopExecution(3)){break;}if (window.CP.shouldStopExecution(3)){break;}if (window.CP.shouldStopExecution(3)){break;}if (window.CP.shouldStopExecution(3)){break;}if (window.CP.shouldStopExecution(3)){break;}if (window.CP.shouldStopExecution(3)){break;}if (window.CP.shouldStopExecution(3)){break;}if (window.CP.shouldStopExecution(3)){break;}if (window.CP.shouldStopExecution(3)){break;}if (window.CP.shouldStopExecution(3)){break;}if (window.CP.shouldStopExecution(3)){break;}if (window.CP.shouldStopExecution(3)){break;}if (window.CP.shouldStopExecution(3)){break;}if (window.CP.shouldStopExecution(3)){break;}if (window.CP.shouldStopExecution(3)){break;}if (window.CP.shouldStopExecution(3)){break;}
+      args[_key3] = arguments[_key3];
+    }
+window.CP.exitedLoop(3);
+
+window.CP.exitedLoop(3);
+
+window.CP.exitedLoop(3);
+
+window.CP.exitedLoop(3);
+
+window.CP.exitedLoop(3);
+
+window.CP.exitedLoop(3);
+
+window.CP.exitedLoop(3);
+
+window.CP.exitedLoop(3);
+
+window.CP.exitedLoop(3);
+
+window.CP.exitedLoop(3);
+
+window.CP.exitedLoop(3);
+
+window.CP.exitedLoop(3);
+
+window.CP.exitedLoop(3);
+
+window.CP.exitedLoop(3);
+
+window.CP.exitedLoop(3);
+
+window.CP.exitedLoop(3);
+
+window.CP.exitedLoop(3);
+
+window.CP.exitedLoop(3);
+
+window.CP.exitedLoop(3);
+
+window.CP.exitedLoop(3);
+
+window.CP.exitedLoop(3);
+
+window.CP.exitedLoop(3);
+
+window.CP.exitedLoop(3);
+
+window.CP.exitedLoop(3);
+
+window.CP.exitedLoop(3);
+
+window.CP.exitedLoop(3);
+
+window.CP.exitedLoop(3);
+
+window.CP.exitedLoop(3);
+
+window.CP.exitedLoop(3);
+
+window.CP.exitedLoop(3);
+
+window.CP.exitedLoop(3);
+
+window.CP.exitedLoop(3);
+
+
+    return fns.reverse().reduce(function (x, f) {
+      return f.apply(f, [].concat(x));
+    }, args);
+  };
+};
